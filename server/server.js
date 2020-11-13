43e0e824fdb76836b1ffd1e139b41ac102e3b81e
Steven Maloney <steven.maloney@gmail.com>
2020-11-12 21:39:54 -0600

    added get and post routes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,24 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
+
+let stringMathPhrase = [];
+
+//GET route
+app.get('/calculate', (req, res) => {
+    console.log('getting calculation...');
+    res.send(stringMathPhrase);
+});
+
+//POST route
+app.post('/calculate', (req, res) => {
+    console.log('posting math pieces...');
+    let postStuff = req.body;
+    stringMathPhrase.push(postStuff);
+    res.sendStatus(200);
+})
+
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
